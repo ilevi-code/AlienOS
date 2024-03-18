715456031f079efe4a2bdda4049e8d473f0d06c4
ilevi <itayill01@gmail.com>
2024-03-19 01:30:27 +0200

    Use store a pointer in heap internally
    Make use of builtin functions and check for alignment and stuff

--- src/kalloc.rs
+++ src/kalloc.rs
@@ -1,18 +1,18 @@
+use crate::mmu;
 use core::cell::UnsafeCell;
 use core::mem::MaybeUninit;
-use crate::mmu;
 
 // The first 1GB is MMIO
 const PHYS_START: usize = 0x40000000;
 pub const BLOCK_SIZE: usize = 0x4000;
 
 struct FreePage {
-    next: Option<*mut FreePage>,
+    next: Option<&'static mut FreePage>,
 }
 
 struct PageAllocator {
-    head: usize,
-    free_list: Option<*mut FreePage>,
+    head: *mut u8,
+    free_list: Option<&'static mut FreePage>,
 }
 
 unsafe impl Sync for PageAllocator {}
@@ -23,19 +23,26 @@ struct Cell<T> {
 
 unsafe impl<T> Sync for Cell<T> {}
 
+// TODO Box like blocks with lifetimes
 impl PageAllocator {
     pub fn alloc_frame(&mut self) -> usize {
         // TODO check free_list. We need temporary access to the page.
         // TODO check we do not exceed the amount of available RAM
-        let old_head = self.head;
-        self.head = old_head + BLOCK_SIZE;
-        old_head
+        // TODO p2v and memset the page
+        unsafe {
+            let new_head = self.head.add(BLOCK_SIZE);
+            let block_ptr = core::mem::replace(&mut self.head, new_head);
+            block_ptr.write_bytes(0, BLOCK_SIZE);
+            block_ptr as usize
+        }
     }
 
-    pub unsafe fn free_frame(&mut self, page: usize) {
-        let page = page as *mut FreePage;
-        (*page).next = self.free_list;
-        self.free_list = Some(page);
+    pub fn free_frame(&mut self, page_addr: usize) {
+        let page_ptr = page_addr as *mut FreePage;
+        if let Some(page) = unsafe { page_ptr.as_mut() } {
+            page.next = self.free_list.take();
+            self.free_list = Some(page);
+        };
     }
 }
 
@@ -47,7 +54,7 @@ pub fn init() {
     let phys_head = PHYS_START + mmu::get_kernel_location().len();
     unsafe {
         (*PAGE_ALLOCATOR.inner.get()).write(PageAllocator {
-            head: phys_head,
+            head: phys_head as *mut u8,
             free_list: None,
         });
     }
