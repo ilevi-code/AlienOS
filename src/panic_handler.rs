f60a413cdb7bcb1b245695aa77003b01ac38001b
ilevi <itayill01@gmail.com>
2024-08-13 00:55:03 +0300

    Improve panic code

--- src/panic_handler.rs
+++ src/panic_handler.rs
@@ -1,6 +1,26 @@
-use crate::console;
-use core::arch::asm;
 use core::panic::PanicInfo;
+
+use crate::console;
+
+mod semihosting {
+    use core::arch::asm;
+
+    pub fn shutdown() -> ! {
+        unsafe {
+            // Semihosting call.
+            // 0x18 is angel_SWIreason_ReportException
+            // 0x20026 is ADP_Stopped_ApplicationExit
+            asm!(
+                "MOV r0, #0x18",
+                "MOVT r1, #2",
+                "MOV r1, #0x26",
+                "SVC #0x00123456",
+            );
+        }
+        // In case something failed - hang.
+        loop {}
+    }
+}
 
 #[panic_handler]
 unsafe fn panic_handler(panic_info: &PanicInfo) -> ! {
@@ -8,16 +28,5 @@ unsafe fn panic_handler(panic_info: &PanicInfo) -> ! {
     if res.is_err() {
         console::write("\n--- error formatting panic info ---\n");
     }
-    unsafe {
-        // Semihosting call.
-        // 0x18 is angel_SWIreason_ReportException
-        // 0x20026 is ADP_Stopped_ApplicationExit
-        asm!(
-            "MOV r0, #0x18",
-            "MOVT r1, #2",
-            "MOV r1, #0x26",
-            "SVC #0x00123456",
-        );
-    }
-    loop {}
+    semihosting::shutdown()
 }
