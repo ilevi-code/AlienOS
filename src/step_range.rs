a0cb669c1c413d932599a0ceb526634fcde22ba8
ilevi <itayill01@gmail.com>
2025-04-30 17:47:14 +0300

    Add tests for step-range

--- src/step_range.rs
+++ src/step_range.rs
@@ -4,24 +4,29 @@ use core::ops::{Add, Range};
 
 use crate::num::{AlignDown, AlignUp, OverflowingAdd};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct StepRange<T> {
-    pub start: T,
+    pub start: Option<T>,
     pub end: T,
     pub step: T,
 }
 
 impl<T> StepRange<T> {
     pub fn new(start: T, end: T, step: T) -> Self {
-        Self { start, end, step }
+        Self {
+            start: Some(start),
+            end: end,
+            step,
+        }
     }
 }
 
 impl<T: AlignUp + AlignDown + Copy> StepRange<T> {
     pub fn align_from(range: Range<T>, step: T) -> Self {
+        let end = range.end.align_down(step);
         Self {
-            start: range.start.align_down(step),
-            end: range.end.align_up(step),
+            start: Some(range.start.align_down(step)),
+            end: end,
             step,
         }
     }
@@ -31,13 +36,38 @@ impl<T: Add<Output = T> + Ord + Copy + OverflowingAdd> Iterator for StepRange<T>
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.start < self.end {
+        let Some(start) = self.start else {
+            return None;
+        };
+        if start > self.end {
             return None;
         }
-        let (n, overflow) = self.start.overflowing_add(self.step);
+        let (n, overflow) = start.overflowing_add(self.step);
         if overflow {
-            return None;
+            return mem::replace(&mut self.start, None);
         }
-        Some(mem::replace(&mut self.start, n))
+        mem::replace(&mut self.start, Some(n))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_case]
+    fn normal_range() {
+        let mut range = StepRange::align_from(0..5, 2);
+        assert_eq!(range.next(), Some(0));
+        assert_eq!(range.next(), Some(2));
+        assert_eq!(range.next(), Some(4));
+        assert_eq!(range.next(), None);
+    }
+
+    #[test_case]
+    fn overflowing_range() {
+        let mut range = StepRange::align_from(0xffff_ffe0..0xffff_fff1, 0x10);
+        assert_eq!(range.next(), Some(0xffff_ffe0));
+        assert_eq!(range.next(), Some(0xffff_fff0));
+        assert_eq!(range.next(), None);
     }
 }
