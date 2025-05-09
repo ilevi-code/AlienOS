1192ff9dfc19d702d9cc98fbd5b78e6d8df2c21d
ilevi <itayill01@gmail.com>
2025-05-09 09:04:59 +0300

    Support more MMU registers

--- src/arch.rs
+++ src/arch.rs
@@ -13,3 +13,31 @@ pub fn set_ttbr0(table: usize) {
         asm!("MCR p15, 0, {table}, c2, c0, 0", table = in(reg) table);
     }
 }
+
+pub fn get_ttbr1() -> usize {
+    let table;
+    unsafe {
+        asm!("MRC p15, 0, {table}, c2, c0, 1", table = out(reg) table);
+    }
+    table
+}
+
+pub fn set_ttbr1(table: usize) {
+    unsafe {
+        asm!("MCR p15, 0, {table}, c2, c0, 1", table = in(reg) table);
+    }
+}
+
+pub fn get_ttbcr() -> usize {
+    let value;
+    unsafe {
+        asm!("MRC p15, 0, {}, c2, c0, 2", out(reg) value);
+    }
+    value
+}
+
+pub fn set_ttbcr(value: usize) {
+    unsafe {
+        asm!("MCR p15, 0, {}, c2, c0, 2", in(reg) value);
+    }
+}
