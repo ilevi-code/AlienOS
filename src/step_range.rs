e4ea1b97576010c181375b5f29a45db2c833fa26
ilevy <itayill01@gmail.com>
2025-08-29 23:47:54 +0300

    Fix inclusion of step range
    Cause two pages to be mapped by when asking to map only a single page

--- src/step_range.rs
+++ src/step_range.rs
@@ -15,7 +15,7 @@ impl<T> StepRange<T> {
     pub fn new(start: T, end: T, step: T) -> Self {
         Self {
             start: Some(start),
-            end: end,
+            end,
             step,
         }
     }
@@ -23,10 +23,9 @@ impl<T> StepRange<T> {
 
 impl<T: AlignUp + AlignDown + Copy> StepRange<T> {
     pub fn align_from(range: Range<T>, step: T) -> Self {
-        let end = range.end.align_down(step);
         Self {
             start: Some(range.start.align_down(step)),
-            end: end,
+            end: range.end,
             step,
         }
     }
@@ -36,17 +35,16 @@ impl<T: Add<Output = T> + Ord + Copy + OverflowingAdd> Iterator for StepRange<T>
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let Some(start) = self.start else {
-            return None;
-        };
-        if start > self.end {
+        let start = self.start?;
+        if start >= self.end {
             return None;
         }
         let (n, overflow) = start.overflowing_add(self.step);
         if overflow {
-            return mem::replace(&mut self.start, None);
+            self.start.take()
+        } else {
+            self.start.replace(n)
         }
-        mem::replace(&mut self.start, Some(n))
     }
 }
 
@@ -60,6 +58,13 @@ mod tests {
         assert_eq!(range.next(), Some(0));
         assert_eq!(range.next(), Some(2));
         assert_eq!(range.next(), Some(4));
+        assert_eq!(range.next(), None);
+    }
+
+    #[test_case]
+    fn non_inclusive() {
+        let mut range = StepRange::align_from(0..2, 2);
+        assert_eq!(range.next(), Some(0));
         assert_eq!(range.next(), None);
     }
 
