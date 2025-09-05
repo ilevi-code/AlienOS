744e8204828a2363d99a49bad9c2e9d9643947d4
ilevy <itayill01@gmail.com>
2025-09-05 16:02:51 +0300

    Support setting stack for different PE modes

--- src/arch.rs
+++ src/arch.rs
@@ -41,3 +41,34 @@ pub fn set_ttbcr(value: usize) {
         asm!("MCR p15, 0, {}, c2, c0, 2", in(reg) value);
     }
 }
+
+pub fn get_cpsr() -> usize {
+    let value;
+    unsafe {
+        asm!("MRS {}, CPSR", out(reg) value);
+    }
+    value
+}
+
+pub enum PeMode {
+    User = 0b0000,
+    Fiq = 0b0001,
+    Irq = 0b0010,
+    Supervisor = 0b0011,
+    Abort = 0b0111,
+}
+
+pub fn set_stack_for_pe(stack: usize, mode: PeMode) {
+    let cpsr = get_cpsr();
+    let request_cpsr = (cpsr & !0xf) | mode as usize;
+    unsafe {
+        core::arch::asm!(
+            "msr CPSR_c, {request_cpsr}",
+            "mov sp, {stack}",
+            "msr CPSR_c, {cpsr}",
+            stack = in(reg) stack,
+            request_cpsr = in(reg) request_cpsr,
+            cpsr = in(reg) cpsr,
+        );
+    }
+}
