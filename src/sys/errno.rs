8a3ec454ce92a18c01e3906d4d0bb1ab004a0fbc
ilevy <itayill01@gmail.com>
2026-03-22 23:39:47 +0200

    Convert between kernel error and errno

--- src/sys/errno.rs
+++ src/sys/errno.rs
@@ -1,5 +1,26 @@
+use crate::error::Error;
+
 pub enum Errno {
     Fault,
     InvalidFd,
     NoSyscall,
+    OutOfMem,
+    NoSuchDevice,
+    Internal,
+}
+
+impl From<crate::error::Error> for Errno {
+    fn from(value: crate::error::Error) -> Self {
+        match value {
+            Error::OutOfMem => Errno::OutOfMem,
+            Error::MemoryFault => Errno::Fault,
+            Error::NoDevice => Errno::NoSuchDevice,
+            Error::PerCpuReborrow
+            | Error::NoCurrentProcess
+            | Error::Unsupproted
+            | Error::OutOfRange
+            | Error::Remap
+            | Error::LayoutError(_) => Errno::Internal,
+        }
+    }
 }
