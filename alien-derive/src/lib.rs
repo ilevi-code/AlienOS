ffc2b773426c300a3291ab757fb27463a2440133
ilevy <itayill01@gmail.com>
2026-04-02 23:29:38 +0300

    Create IntEnum macro

--- alien-derive/src/lib.rs
+++ alien-derive/src/lib.rs
@@ -1,9 +1,9 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Pod)]
-pub fn derive_my_marker(input: TokenStream) -> TokenStream {
+pub fn derive_pod(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let name = input.ident;
@@ -25,7 +25,7 @@ pub fn derive_my_marker(input: TokenStream) -> TokenStream {
         _ => {
             return TokenStream::from(
                 syn::Error::new(name.span(), "Only structs can derive `Pod`").to_compile_error(),
-            )
+            );
         }
     };
 
@@ -35,6 +35,72 @@ pub fn derive_my_marker(input: TokenStream) -> TokenStream {
         where
             #( #field_types: Pod ),*
         {}
+    };
+
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(IntEnum, attributes(default))]
+pub fn derive_int_enum(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let mut variant_idents = vec![];
+    let mut variant_discriminants = vec![];
+    let mut default_variant = None;
+
+    match input.data {
+        Data::Enum(data_enum) => {
+            for variant in data_enum.variants {
+                match &variant.fields {
+                    syn::Fields::Unit => (),
+                    _ => {
+                        return TokenStream::from(
+                            syn::Error::new(
+                                variant.fields.span(),
+                                "IntEnum's variants must be unit-types",
+                            )
+                            .to_compile_error(),
+                        );
+                    }
+                }
+                let is_default = variant
+                    .attrs
+                    .iter()
+                    .any(|attr| attr.path().is_ident("default"));
+
+                if is_default {
+                    default_variant = Some(variant.ident);
+                } else {
+                    variant_idents.push(variant.ident);
+                    variant_discriminants.push(variant.discriminant.unwrap().1);
+                }
+            }
+        }
+        _ => {
+            return TokenStream::from(
+                syn::Error::new(name.span(), "Only enums can derive `IntEnum`").to_compile_error(),
+            );
+        }
+    };
+
+    if default_variant.is_none() {
+        return TokenStream::from(
+            syn::Error::new(name.span(), "missing #[default] variant").to_compile_error(),
+        );
+    }
+
+    let expanded = quote! {
+        impl core::convert::From<u32> for #name
+        {
+            fn from(value: u32) -> Self {
+                 match value {
+                     #( #variant_discriminants => Self::#variant_idents,)*
+                     _ => Self::#default_variant,
+                 }
+            }
+        }
     };
 
     TokenStream::from(expanded)
--- src/fs/ext2/revision.rs
+++ src/fs/ext2/revision.rs
@@ -1,18 +1,9 @@
-#[derive(Clone, Copy)]
+use alien_derive::IntEnum;
+
+#[derive(Clone, Copy, IntEnum)]
 pub enum Revision {
     GoodOld = 0,
     Dynamic = 1,
-    Unknown = 2,
-}
-
-impl From<u32> for Revision {
-    fn from(value: u32) -> Self {
-        if value == Revision::GoodOld as u32 {
-            Revision::GoodOld
-        } else if value == Revision::Dynamic as u32 {
-            Revision::Dynamic
-        } else {
-            Revision::Unknown
-        }
-    }
+    #[default]
+    Unknown,
 }
