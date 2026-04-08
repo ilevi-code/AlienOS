af453096358faf0ac2dbe790f647c20d9e56b791
ilevy <itayill01@gmail.com>
2026-04-08 15:35:35 +0300

    report the exit code

--- src/sys/exit.rs
+++ src/sys/exit.rs
@@ -2,10 +2,10 @@ use crate::{interrupts::RegSet, sched::with_current, sys::SyscallResult, syscall
 
 syscall!(exit);
 
-fn exit(_regs: &mut RegSet) -> SyscallResult {
+fn exit(regs: &mut RegSet) -> SyscallResult {
     with_current(|current| {
         if current.pid == 1 {
-            panic!("Init exited!");
+            panic!("Init exited! (code={})", regs.r[1]);
         }
     })?;
     todo!();
