08044c5ecbe6529c06b8bbd6c7ce080a01cf77cb
ilevy <itayill01@gmail.com>
2026-04-08 17:14:05 +0300

    Open /dev/console in init

--- init/src/main.rs
+++ init/src/main.rs
@@ -2,8 +2,9 @@
 #![no_main]
 
 const SYS_EXIT: usize = 2;
+const SYS_OPEN: usize = 3;
 
-use core::{arch::asm, panic::PanicInfo};
+use core::{arch::asm, panic::PanicInfo, result::Result};
 
 #[panic_handler]
 fn panic_handler(_painc_info: &PanicInfo) -> ! {
@@ -15,7 +16,42 @@ fn exit(exit_code: u32) -> ! {
     unreachable!()
 }
 
+enum Errno {
+    Unknown,
+}
+
+fn open(path: &str) -> Result<usize, Errno> {
+    let result: i32;
+    let fd;
+    unsafe {
+        asm!(
+            "svc #0",
+            "mov {result}, r0",
+            "mov {fd}, r1",
+            result = out(reg) result,
+            fd = out(reg) fd,
+            in("r0") SYS_OPEN,
+            in("r1") path.as_ptr(),
+            in("r2") path.len()
+        )
+    }
+    if result == 0 {
+        Ok(fd)
+    } else {
+        Err(Errno::Unknown)
+    }
+}
+
 #[unsafe(no_mangle)]
 extern "C" fn _init() {
-    exit(0)
+    let exit_code = match main() {
+        Ok(_) => 0,
+        Err(_) => 1,
+    };
+    exit(exit_code)
+}
+
+fn main() -> Result<(), Errno> {
+    let _console = open("/dev/console")?;
+    Ok(())
 }
